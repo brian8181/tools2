3ae176211f792ebdca4d589c470e08a154b1d9ca
Brian K Preston <brian8181@gmail.com>
2026-08-18 09:05:47 -0500

    fixed CMakelist.txt ...

---
+++ cbuild/CMakeFiles/tools2_a.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/brian/src/tools2/src/fileio.cpp" "CMakeFiles/tools2_a.dir/src/fileio.cpp.o" "gcc" "CMakeFiles/tools2_a.dir/src/fileio.cpp.o.d"
+  "/home/brian/src/tools2/src/logger.cpp" "CMakeFiles/tools2_a.dir/src/logger.cpp.o" "gcc" "CMakeFiles/tools2_a.dir/src/logger.cpp.o.d"
+  "/home/brian/src/tools2/src/loop.cpp" "CMakeFiles/tools2_a.dir/src/loop.cpp.o" "gcc" "CMakeFiles/tools2_a.dir/src/loop.cpp.o.d"
+  "/home/brian/src/tools2/src/singleton.cpp" "CMakeFiles/tools2_a.dir/src/singleton.cpp.o" "gcc" "CMakeFiles/tools2_a.dir/src/singleton.cpp.o.d"
+  "/home/brian/src/tools2/src/utest.cpp" "CMakeFiles/tools2_a.dir/src/utest.cpp.o" "gcc" "CMakeFiles/tools2_a.dir/src/utest.cpp.o.d"
+  "/home/brian/src/tools2/src/utility.cpp" "CMakeFiles/tools2_a.dir/src/utility.cpp.o" "gcc" "CMakeFiles/tools2_a.dir/src/utility.cpp.o.d"
+  "/home/brian/src/tools2/src/variant.cpp" "CMakeFiles/tools2_a.dir/src/variant.cpp.o" "gcc" "CMakeFiles/tools2_a.dir/src/variant.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cbuild/CMakeFiles/tools2_a.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tools2_a.dir/src/fileio.cpp.o"
+  "CMakeFiles/tools2_a.dir/src/fileio.cpp.o.d"
+  "CMakeFiles/tools2_a.dir/src/logger.cpp.o"
+  "CMakeFiles/tools2_a.dir/src/logger.cpp.o.d"
+  "CMakeFiles/tools2_a.dir/src/loop.cpp.o"
+  "CMakeFiles/tools2_a.dir/src/loop.cpp.o.d"
+  "CMakeFiles/tools2_a.dir/src/singleton.cpp.o"
+  "CMakeFiles/tools2_a.dir/src/singleton.cpp.o.d"
+  "CMakeFiles/tools2_a.dir/src/utest.cpp.o"
+  "CMakeFiles/tools2_a.dir/src/utest.cpp.o.d"
+  "CMakeFiles/tools2_a.dir/src/utility.cpp.o"
+  "CMakeFiles/tools2_a.dir/src/utility.cpp.o.d"
+  "CMakeFiles/tools2_a.dir/src/variant.cpp.o"
+  "CMakeFiles/tools2_a.dir/src/variant.cpp.o.d"
+  "libtools2_a.a"
+  "libtools2_a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tools2_a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
